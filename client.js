e568d7ce7f2f72f35647969a792377ed5338c36e
Luke Schlangen <lukeschlangen@gmail.com>
2017-02-13 11:37:28 -0600

    Deleting employee salary from total on delete

--- client.js
+++ client.js
@@ -31,6 +31,14 @@ $(document).ready(function(){ // waits for DOM to completely load
 
   // Adding listener for clicking delete employee buttons
   $('#employeeTableBody').on('click', '.deleteEmployeeButton', function(){
-    $(this).parent().parent().remove(); // selecting the row that I want to delete
+    // Removing employee salary from total
+    var deletedEmployeeSalary = $(this).parent().prev().text();
+    var deletedEmployeeMonthlyExpenses = deletedEmployeeSalary / 12;
+    var previousMonthlyExpenses = $('#monthlyExpenses').text();
+    var newTotalMonthlyExpenses = previousMonthlyExpenses - deletedEmployeeMonthlyExpenses;
+    $('#monthlyExpenses').text(newTotalMonthlyExpenses);
+
+    // Selecting and deleting employee row from table
+    $(this).parent().parent().remove();
   });
 });
