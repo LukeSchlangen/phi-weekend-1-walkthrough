8ca4d6e99f4b1140976cb30b9d20edab854d860a
Luke Schlangen <lukeschlangen@gmail.com>
2017-02-14 09:52:11 -0600

    Converting to jquery form

--- client.js
+++ client.js
@@ -1,21 +1,41 @@
 $(document).ready(function(){ // waits for DOM to completely load
-  $('#submitNewEmployee').on('click', function(){ // event listener on submitNewEmployee Button
-    // Declaring variables and retrieving values from input boxes
-    var firstName = $('#firstName').val();
-    var lastName = $('#lastName').val();
-    var idNumber = $('#idNumber').val();
-    var jobTitle = $('#jobTitle').val();
-    var annualSalary = $('#annualSalary').val();
+  $('form').on('submit', function(event){ // event listener on form submission
+    event.preventDefault(); // Do not bring us to a new page
+
+    // Create an array of the inputs, the inputs are converted to objects
+    // The objects have two properties, name and value
+    // e.g. {name: 'firstName', value: 'Luke'}
+    console.log('form values: ', $(this).serializeArray());
+
+    var submissionArray = $(this).serializeArray(); // [{}, {}, {}]
+    var newEmployeeObject = {}; // {firstName: 'Luke', lastName: 'Schlangen'}
+
+    // submissionArray.forEach(function(inputFieldObject){
+    //   // newEmployeeObject is {}
+    //   newEmployeeObject[inputFieldObject.name] = inputFieldObject.value;
+    //   // newEmployeeObject.firstName = Luke;
+    //   // newEmployeeObject is {firstName: 'Luke'}
+    //   // 2nd Time Through newEmployeeObject is {firstName: 'Luke', lastName: 'Schlangen'}
+    // });
+
+    for(var i = 0; i < submissionArray.length; i++){
+      var inputFieldObject = submissionArray[i];
+      newEmployeeObject[inputFieldObject.name] = inputFieldObject.value;
+      // newEmployeeObject.firstName
+      // newEmployeeObject['firstName']
+    }
+
+    console.log('New Employee Object: ', newEmployeeObject);
 
     // adds new employee row to DOM
     $('#employeeTableBody').append(
       '<tr>' +
-        '<td>' + firstName + '</td>' +
-        '<td>' + lastName + '</td>' +
-        '<td>' + idNumber + '</td>' +
-        '<td>' + jobTitle + '</td>' +
-        '<td>' + annualSalary + '</td>' +
-        '<td><button class="deleteEmployeeButton" data-salary="' + annualSalary + '">Delete ' + firstName + '</button></td>' +
+        '<td>' + newEmployeeObject.firstName + '</td>' +
+        '<td>' + newEmployeeObject.lastName + '</td>' +
+        '<td>' + newEmployeeObject.idNumber + '</td>' +
+        '<td>' + newEmployeeObject.jobTitle + '</td>' +
+        '<td>' + newEmployeeObject.annualSalary + '</td>' +
+        '<td><button class="deleteEmployeeButton" data-salary="' + newEmployeeObject.annualSalary + '">Delete ' + newEmployeeObject.firstName + '</button></td>' +
       '</tr>'
       );
 
